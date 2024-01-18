c967ac141f55124972f434fc339ccd10ed714e0e
danieleangeletti <angelettidaniele@gmail.com>
2024-01-18 18:18:07 +0000

    header con v-for

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,40 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      nav: [
+        {
+          name: "CHARACTERS",
+        },
+        {
+          name: "COMICS",
+        },
+        {
+          name: "MOVIES",
+        },
+        {
+          name: "TV",
+        },
+        {
+          name: "GAMES",
+        },
+        {
+          name: "COLLECTIBLES",
+        },
+        {
+          name: "VIDEOS",
+        },
+        {
+          name: "FANS",
+        },
+        {
+          name: "NEWS",
+        },
+        {
+          name: "SHOPS",
+        },
+      ],
+    };
   },
   methods: {},
 };
@@ -16,7 +49,14 @@ export default {
             <img class="h-75" src="../assets/img/dc-logo.png" alt="" />
           </div>
         </div>
-        <div>lol</div>
+        <div class="h-100 centered-vertically-div">
+          <div
+            v-for="(elem, i) in nav"
+            class="h-100 centered-vertically-div ps-8 pe-8 smaller with-cursor-pointer"
+          >
+            {{ elem.name }}
+          </div>
+        </div>
       </div>
     </div>
   </header>
@@ -30,5 +70,14 @@ export default {
 }
 .h-75 {
   height: 75%;
+}
+.ps-8 {
+  padding-left: 8px;
+}
+.pe-8 {
+  padding-right: 8px;
+}
+.smaller {
+  font-size: 0.8em;
 }
 </style>
