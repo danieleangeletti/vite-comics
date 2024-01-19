53f63e5bf6a73474a4843db6f0303c42a49cf7c5
danieleangeletti <angelettidaniele@gmail.com>
2024-01-19 14:10:22 +0000

    img inserite tramite props (con grafica schifosa)

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -9,4 +9,19 @@ export default {
 };
 </script>
 
-<template></template>
+<template>
+  <div class="single-card">
+    <div>
+      <img :src="product.thumb" :alt="product.series" class="w-100" />
+
+      <h6>
+        {{ product.series }}
+      </h6>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use "../assets/scss/partials/variables.scss" as *;
+@use "../assets/scss/partials/mixins.scss" as *;
+</style>
